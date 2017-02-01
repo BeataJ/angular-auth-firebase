d228e813e7751978cc12447d935d6dc72fca42b1
BeataJ <beata@emc2wave.com>
2017-02-01 13:35:27 -0800

    app and login component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,35 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AngularFireModule } from 'angularfire2';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { EmailComponent } from './email/email.component';
+import { SignupComponent } from './signup/signup.component';
+import { MembersComponent } from './members/members.component';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyDk8i-XCIr3kMZXcze64T9Bkyt6o84Mf0Q",
+  authDomain: "angular-auth-with-firebase.firebaseapp.com",
+  databaseURL: "https://angular-auth-with-firebase.firebaseio.com",
+  storageBucket: "angular-auth-with-firebase.appspot.com",
+  messagingSenderId: "1021739572828"
+}
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    EmailComponent,
+    SignupComponent,
+    MembersComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
