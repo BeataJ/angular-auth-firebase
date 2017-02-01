17504db2652de1f947a4c4fbbf77f6b1cb98475a
BeataJ <beata@emc2wave.com>
2017-02-01 14:41:59 -0800

    errors corrected

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, HostBinding } from '@angular/core';
 import { AngularFire, AuthProviders, AuthMethods } from 'angularfire2';
 import { Router } from '@angular/router';
-import { moveIn } from '../router/animations';
+import { moveIn } from '../router.animations';
 
 @Component({
   selector: 'app-login',
